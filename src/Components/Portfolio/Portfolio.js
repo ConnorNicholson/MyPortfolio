cb19bd6f42efe4d345d633c9a4fccb8dce57adc9
Connor Nicholson <connorjnicholson1@gmail.com>
2019-12-09 13:28:48 -0500

    Adds styling for Portfolio page

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -7,34 +7,40 @@ export default function Portfolio() {
             <section className="portfolio_header">
                 <div className="portfolio_flex_container">
                     <div className="portfolio_box">
-                        <a className="project_link" href="https://github.com/ConnorNicholson/JS-Calculator" target="_blank" rel="noopener noreferrer">
-                            <div className="javascript_project">
-                                <p className="language_icon">JS</p>
-                                <p className="project_name">Calculator</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="portfolio_box">
-                        <a className="project_link" href="https://github.com/ConnorNicholson/NodeJS-Employee-Endpoints" target="_blank" rel="noopener noreferrer">
-                            <div className="nodeJS_project">
-                                <p className="language_icon">NodeJS</p>
-                                <p className="project_name">Employee Endpoints</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="portfolio_box">
-                        <a className="project_link" href="https://github.com/ConnorNicholson/React-State-and-Components" target="_blank" rel="noopener noreferrer">
-                            <div className="react_project">
-                                <p className="language_icon">React</p>
-                                <p className="project_name">State and Components</p>
+                        {/* target="_blank" has a vulnerability. Use rel="noopener noreferrer" to prevent phishing attacks / browser hijacking */}
+                        <a className="project_link" href="https://codepen.io/ConnorN/project/full/XJbENa" target="_blank" rel="noopener noreferrer">
+                            <div className="html_project">
+                                <img className="project_logo" src="https://res.cloudinary.com/connorn/image/upload/v1575904333/Portfolio%20Images/html_logo_hh2uym.png" alt="project_logo" />
                             </div>
                         </a>
                     </div>
                     <div className="portfolio_box">
                         <a className="project_link" href="https://github.com/ConnorNicholson/MySQL-Cars-Database" target="_blank" rel="noopener noreferrer">
                             <div className="mysql_project">
-                                <p className="language_icon">MySQL</p>
-                                <p className="project_name">Cars Database</p>
+                                <img className="mysql_logo" src="https://res.cloudinary.com/connorn/image/upload/v1575903509/Portfolio%20Images/mysql_dolphin_logo_ctzoip.png" alt="project_logo" />
+                            </div>
+                        </a>
+                    </div>
+                    <div className="portfolio_box">
+                        <a className="project_link" href="https://github.com/ConnorNicholson/NodeJS-Employee-Endpoints" target="_blank" rel="noopener noreferrer">
+                            <div className="nodeJS_project">
+                                <img className="project_logo" src="https://res.cloudinary.com/connorn/image/upload/v1575900573/Portfolio%20Images/nodejs_logo_tepdl0.svg" alt="project_logo" />
+                            </div>
+                        </a>
+                    </div>
+                    <div className="portfolio_box">
+                        <a className="project_link">
+                            <div className="react_project">
+                                <img className="project_logo react_logo" src="https://res.cloudinary.com/connorn/image/upload/v1575901647/Portfolio%20Images/react_logo_mzolsv.png" alt="project_logo" />
+                                {/* <p className="language_icon">React</p>
+                                <p className="project_name">State and Components</p> */}
+                            </div>
+                        </a>
+                    </div>
+                    <div className="portfolio_box">
+                        <a className="project_link" href="https://react-proj6-pwa.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                            <div className="pwa_project">
+                                <img className="project_logo" src="https://res.cloudinary.com/connorn/image/upload/v1575904210/Portfolio%20Images/pwa_logo_nyecba.png" alt="project_logo" />
                             </div>
                         </a>
                     </div>
