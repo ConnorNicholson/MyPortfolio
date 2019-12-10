6cc32b730e38095957105a474ac72d07f0837984
Connor Nicholson <connorjnicholson1@gmail.com>
2019-12-10 12:08:51 -0500

    Updated styling for Portfolio

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,7 +18,6 @@ export default function Footer() {
                     </div>
                 </div>
             </footer>
-
         </div>
     );
 };
--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -13,14 +13,15 @@ export default function Portfolio() {
                                 <img className="project_logo" src="https://res.cloudinary.com/connorn/image/upload/v1575904333/Portfolio%20Images/html_logo_hh2uym.png" alt="project_logo" />
                             </div>
                         </a>
+                        <span className="project_text">Gateway Travel Site</span>
                     </div>
-                    <div className="portfolio_box">
+                    {/* <div className="portfolio_box">
                         <a className="project_link" href="https://github.com/ConnorNicholson/MySQL-Cars-Database" target="_blank" rel="noopener noreferrer">
                             <div className="mysql_project">
                                 <img className="mysql_logo" src="https://res.cloudinary.com/connorn/image/upload/v1575903509/Portfolio%20Images/mysql_dolphin_logo_ctzoip.png" alt="project_logo" />
                             </div>
                         </a>
-                    </div>
+                    </div> */}
                     <div className="portfolio_box">
                         <a className="project_link" href="https://github.com/ConnorNicholson/NodeJS-Employee-Endpoints" target="_blank" rel="noopener noreferrer">
                             <div className="nodeJS_project">
@@ -32,8 +33,13 @@ export default function Portfolio() {
                         <a className="project_link" href="https://cjn-portfolio.herokuapp.com/" target="_blank" rel="noopener noreferrer">
                             <div className="react_project">
                                 <img className="project_logo react_logo" src="https://res.cloudinary.com/connorn/image/upload/v1575901647/Portfolio%20Images/react_logo_mzolsv.png" alt="project_logo" />
-                                {/* <p className="language_icon">React</p>
-                                <p className="project_name">State and Components</p> */}
+                            </div>
+                        </a>
+                    </div>
+                    <div className="portfolio_box">
+                        <a className="project_link" href="https://github.com/Code2Hire/submit-weeklychallenges-r2h-2019-ConnorNicholson/blob/master/C%23Challenges/VeryHard/Program.cs" target="_blank" rel="noopener noreferrer">
+                            <div className="csharp_project">
+                                <img className="project_logo" src="https://res.cloudinary.com/connorn/image/upload/v1575993308/Portfolio%20Images/csharp_logo_crz6i7.png" alt="project logo" />
                             </div>
                         </a>
                     </div>
